b596f119e710941f6dec35da60abecbbec859970
emoryoakley <emoryoakley.game@gmail.com>
2025-04-11 23:23:14 +0800

    [1.5.20250411] update: 重新編譯 DLL (build version: 1.5.9233.41912)、更新變更日誌與版本號

--- Source/Properties/VersionInfo.cs
+++ Source/Properties/VersionInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 // GenerateVersion.dll 自動生成 VersionInfo.cs
-[assembly: AssemblyVersion("1.5.9232.18013")]
-[assembly: AssemblyFileVersion("1.5.9232.18013")]
-[assembly: AssemblyInformationalVersion("1.5.20250410")]
+[assembly: AssemblyVersion("1.5.9233.41912")]
+[assembly: AssemblyFileVersion("1.5.9233.41912")]
+[assembly: AssemblyInformationalVersion("1.5.20250411")]
